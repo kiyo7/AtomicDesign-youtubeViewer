bf5e585b6c47511bc99b2b45b62336809935a13b
kiyo <kiyo324783@icloud.com>
2021-06-25 23:17:03 +0900

    3章

---
+++ src/components/atoms/Input/index.stories.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { actions } from "@storybook/addon-actions"; //storybookのプラグイン。inputのonChangeのようなアクションを起こすものの動作確認をするために使う
+import Input from ".";
+
+export default { title: "atoms/Input " };
+
+const props = {
+  placeholder: "入力してください",
+  ...actions("onChange"),
+};
+
+export const input = () => <Input {...props} />;
+
+export const defaultValue = () => <Input {...props} defaultValue="ねこ" />;
+defaultValue.story = { name: "デフォルト値" };
