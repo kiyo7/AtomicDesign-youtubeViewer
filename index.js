e8a2fb78cf29928b79c9ae3422ce3eec1df28beb
kiyo <kiyo324783@icloud.com>
2021-06-26 16:31:27 +0900

    4章

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
 const router = express.Router();
 
@@ -8,11 +8,13 @@ const server = app.listen(3000, function () {
   console.log(`Node.js is listening to PORT: ${server.address().port}`);
 });
 
-// 静的ファイルのルーティング
-router.use(express.static('public'));
+app.use("/api", require("./api"));
 
-router.get('*', (req, res, next) =>{
-    res.sendFile(path.join(__dirname, './public/index.html'));
+// 静的ファイルのルーティング
+router.use(express.static("public"));
+
+router.get("*", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.use('/', router);
+app.use("/", router);
